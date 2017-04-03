f79d6cc30313989315fe3cffb2736dee100f381c
Sarrah Vesselov <svesselov@contactnitro.com>
2017-04-03 16:10:05 -0400

    added ad styled Google Map for light theme

--- public/js/common.js
+++ public/js/common.js
@@ -3,6 +3,7 @@
 ***********************************************************/
 /*
     PAGE_1. NAVIGATION
+    PAGE_1. GOOGLE MAP
 */
 
 /***********************************************************
@@ -83,4 +84,100 @@ function hasScrolled() {
     }
 
     lastScrollTop = st;
+}
+
+/***********************************************************
+//                PAGE_2 - GOOGLE MAP
+***********************************************************/
+
+function initMap() {
+  var uluru = {lat: 27.9479796, lng: -82.4577729};
+
+  var styledMapType = new google.maps.StyledMapType(
+      [
+          {
+            "featureType": "landscape.natural",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "color": "#e0efef"
+                }
+            ]
+          },
+          {
+            "featureType": "poi",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "hue": "#1900ff"
+                },
+                {
+                    "color": "#c0e8e8"
+                }
+            ]
+          },
+          {
+            "featureType": "road",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "lightness": 100
+                },
+                {
+                    "visibility": "simplified"
+                }
+            ]
+          },
+            {
+            "featureType": "road",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "visibility": "off"
+                }
+            ]
+          },
+            {
+            "featureType": "transit.line",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "visibility": "on"
+                },
+                {
+                    "lightness": 700
+                }
+            ]
+          },
+            {
+            "featureType": "water",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "color": "#7dcdcd"
+                }
+            ]
+          }
+        ],
+            {name: 'Styled Map'});
+
+
+  var map = new google.maps.Map(document.getElementById('js-map'), {
+    zoom: 13,
+    center: uluru
+  });
+
+  map.mapTypes.set('styled_map', styledMapType);
+  map.setMapTypeId('styled_map');
+
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
 }
